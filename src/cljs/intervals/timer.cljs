8f702050fb3752b5afa7c43bbb1fa01a64df8bc0
Matteo Moci <mox601@users.noreply.github.com>
2020-07-05 11:14:10 +0200

    refactored commands

--- src/cljs/intervals/timer.cljs
+++ src/cljs/intervals/timer.cljs
@@ -1,39 +1,6 @@
 (ns intervals.timer)
 
-;; todo throw if command can't be applied to state
-
-;; commands
-
-(defn init
-  []
-  {:duration 0
-   :status :stopped})
-
-(defn start
-  [timer duration interval-id]
-  ;;TODO only a stopped/completed timer can be started
-  {:duration duration
-   :status :started
-   :interval-id interval-id})
-
-(defn decrement
-  [timer]
-  ;;TODO only a >= 0 counter can be decremented
-  (assoc-in timer [:duration] (- (timer :duration) 1)))
-
-(defn stop
-  [timer]
-  ;;TODO only a started timer can be stopped
-  {:duration (timer :duration)
-   :status :stopped
-   :interval-id nil})
-
-(defn complete
-  [timer]
-  ;;TODO only a started timer with 0 duration can be completed
-  {:duration 0
-   :status :completed
-   :interval-id nil})
+;; TODO throw if command can't be applied to state
 
 ;; queries
 (defn interval-id
@@ -51,4 +18,34 @@
 (defn expired?
   [timer]
   (<= (timer :duration)  0))
+
+;; commands
+(defn init
+  []
+  {:duration 0
+   :status :stopped})
+
+(defn start
+  [timer duration interval-id]
+  ;;TODO only a stopped/completed timer can be started
+  (assoc timer :duration duration :status :started :interval-id interval-id))
+
+(defn decrement
+  [timer]
+  ;;TODO only a >= 0 counter can be decremented
+  (assoc timer :duration (- (timer :duration) 1)))
+
+(defn stop
+  [timer]
+  ;;TODO only a started timer can be stopped
+  (assoc timer :status :stopped :interval-id nil))
+
+;;TODO (throw (js/Error. "only a started timer with 0 duration can be completed"))
+(defn complete
+  [timer]
+  (if (and
+       (started? timer)
+       (expired? timer))
+    (assoc timer :status :completed :interval-id nil)
+    timer))
 
